b083880a214f380873e6446e0fc7a35aeaf60d3d
Nippan <nippanrajak22@gmail.com>
2022-03-24 18:34:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,77 +1,61 @@
-import React, { useState, useRef } from 'react';
-import style from "./App.css"
+import React, { useState, useRef } from "react";
+import "./App.css";
 const App = (e) => {
-  const [isResizing, setIsResizing] = useState(false)
-  const resizerRef = useRef(null)
-  const el = document.querySelector(".item");
-  el.addEventListener("mousedown", mousedown);
+  const [isResizing, setIsResizing] = useState(false);
+  const resizerRef = useRef(null);
+  const boxRef = useRef(null);
+  const currentResizerRef = useRef(null);
+  const sizing = (e) => {
+    console.log("hellooo");
+    currentResizerRef.current = e.target;
+    setIsResizing(true);
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+    
+    function mousemove(e) {
+      const rect = boxRef.current.getBoundingClientRect();
+      console.log(rect, "lola");
 
-  const mousedown=()=>{
-  window.addEventListener("mousemove", mousemove);
-  window.addEventListener("mouseup", mouseup);
-  let prevX = e.clientX;
-  let prevY = e.clientY;
-
-  const mousemove=(e)=>{
-    if(!isResizing){
-      let newX = prevX - e.clientX;
-      let newY = prevY - e.clientY;
-
-      const rect = el.getBoundingClientRect();
-
-      el.style.left = rect.left - newX+prevX;
-      el.style.top = rect.top - newY+prevX;
+      if (currentResizerRef.current.classList.contains("se")) {
+        boxRef.current.style.width = rect.width - (prevX - e.clientX) + "px";
+        boxRef.current.style.height = rect.height - (prevY - e.clientY) + "px";
+      } else if (currentResizerRef.current.classList.contains("sw")) {
+        boxRef.current.style.width = rect.width + (prevX - e.clientX) + "px";
+        boxRef.current.style.height = rect.height - (prevY - e.clientY) + "px";
+        boxRef.current.style.left = rect.left - (prevX - e.clientX) + "px";
+        console.log(rect.left)
+      } else if (currentResizerRef.current.classList.contains("ne")) {
+        boxRef.current.style.width = rect.width - (prevX - e.clientX) + "px";
+        boxRef.current.style.height = rect.height + (prevY - e.clientY) + "px";
+        boxRef.current.style.top = rect.top - (prevY - e.clientY) + "px";
+      } else {
+        boxRef.current.style.width = rect.width + (prevX - e.clientX) + "px";
+        boxRef.current.style.height = rect.height + (prevY - e.clientY) + "px";
+        boxRef.current.style.top = rect.top - (prevY - e.clientY) + "px";
+        boxRef.current.style.left = rect.left - (prevX - e.clientX) + "px";
+      }
 
       prevX = e.clientX;
       prevY = e.clientY;
     }
-  }
-  
-  const mouseup = ()=>{
-    window.removeEventListener("mousemove", mousemove)
-    window.removeEventListener("mouseup", mouseup)
-  }
-}
-
-  const resizers = document.querySelectorAll(".resizer");
-let currentResizer;
-
-for (let resizer of resizers) {
-  resizer.addEventListener("mousedown", mousedown);
-
-  const mousedown =(e)=>{
-    currentResizer=e.target;
-    setIsResizing(true);
-
-    let prevX = e.clientX;
-    let prevY = e.clientY;
+    function mouseup() {
+      window.removeEventListener("mousemove", mousemove);
+      window.removeEventListener("mouseup", mouseup);
+      setIsResizing(false);
+      boxRef.current=null
+    }
 
     window.addEventListener("mousemove", mousemove);
     window.addEventListener("mouseup", mouseup);
+  };
 
-    const mousemove=(e)=>{
-      const rect = el.getBoundingClientRect();
-
-      if(currentResizer){
-      
-      }
-
-      const mouseup=()=>{
-       window.removeEventListener("mousemove", mousemove)
-       window.removeEventListener("mouseup", mouseup);
-       setIsResizing(false)
-      }
-    }
-  }
-}
-
- return (
-    <div class="item">
-     <div ref={resizerRef} class="resizer ne"></div>
-     <div class="resizer nw"></div>
-     <div class="resizer sw"></div>
-     <div class="resizer se"></div>
-   </div>
-  )
-}
+  return (
+    <div ref={boxRef} className="item">
+      <div className="resizer ne" onMouseDown={(e) => sizing(e)}></div>
+      <div className="resizer nw" onMouseDown={(e) => sizing(e)}></div>
+      <div className="resizer sw" onMouseDown={(e) => sizing(e)}></div>
+      <div className="resizer se" onMouseDown={(e) => sizing(e)}></div>
+    </div>
+  );
+};
 export default App;
